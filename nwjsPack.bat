e5b0322f332fbdffd39b22ac4eb29bf6d9a029a6
CubeDoodl <andrewrussell2002@gmail.com>
2015-08-25 13:10:03 -0400

    Added source files

---
+++ nwjsPack.bat
@@ -0,0 +1,9 @@
+mkdir "release"
+del "app.zip"
+del "app.nw"
+"lib/7z.exe" a "app.zip" ".\app\*"
+ren "app.zip" "app.nw"
+copy /b ".\data\nw.exe"+app.nw .\release\app.exe
+copy /b ".\data\*" .\release\
+del "app.nw"
+del ".\release\nw.exe"
